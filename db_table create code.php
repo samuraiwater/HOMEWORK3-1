23d8ee1c752e3f9d7c4f0300f6e6aad0c4a07887
SatosatoNinjya <49612338+SatosatoNinjya@users.noreply.github.com>
2019-05-03 19:52:24 +1000

    create db

---
+++ db_table create code.php
@@ -0,0 +1,20 @@
+//下記のコードを見つけましたがどうやったらRunするかわかりませんでした。。。
+
+
+
+//create database
+
+CREATE DATABASE phptestdb;
+
+//create database
+
+CREATE TABLE Persons (
+  PersonID int,
+  LastName varchar(255),
+  FirstName varchar(255),
+  Address varchar(255),
+  City varchar(255) 
+);
+
+
+?>
